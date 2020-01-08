0ffd6c99d598691fffb91e5ec804ca504d982195
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-08 05:38:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,31 +8,33 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
